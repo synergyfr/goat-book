c0f148f31add74001dcb6609da8e133eac9a3556
Hendrik Willem Rossouw <synergeticfrost@gmail.com>
2024-10-02 16:27:44 +0200

    tests, home, csrf token

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
+
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertContains(response, 'A new list item')
         self.assertTemplateUsed(response, 'home.html')
