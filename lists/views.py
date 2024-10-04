85991c29703e34a314f46e6cda2e10d512a46b02
Hendrik Willem Rossouw <synergeticfrost@gmail.com>
2024-10-04 12:37:49 +0200

    migratations, list model incorpo

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
     return render(request, 'home.html')
+
 
 def view_list(request):
     items = Item.objects.all()
@@ -12,6 +13,8 @@ def view_list(request):
         'items': items
     })
 
+
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    nulist = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=nulist)
     return redirect('/lists/the-only-list-in-the-world/')
