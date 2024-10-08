06a43d450b27118609e26d7e676091ef9fc51b2d
Hendrik Willem Rossouw <synergeticfrost@gmail.com>
2024-10-08 14:42:30 +0200

    Add collectstatic to dockerfile, and new location to gitignore

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,14 @@ FROM python:slim
 RUN python -m venv /venv
 ENV PATH="/venv/bin:$PATH"
 
-RUN pip install "django<6" gunicorn whitenoise
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
 
 COPY src /src
 
 WORKDIR /src
 
+RUN python manage.py collectstatic
+
+ENV DJANGO_DEBUG_FALSE=1
 CMD [ "gunicorn", "--bind", ":8888", "superlists.wsgi:application" ]
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#g2thtys5pygyh4h+1=)rr#m8!fnh7sxu!dyhhs41a66sm#5@@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOST']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
