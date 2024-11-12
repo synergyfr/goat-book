d780d7c5c4c7cc73e3f23de87ac82c26db7b96c9
Hendrik Willem Rossouw <synergeticfrost@gmail.com>
2024-11-12 14:50:25 +0200

    logout and some tests

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from accounts import views
 
 urlpatterns = [
     path('send_login_email', views.send_login_email, name="send_email"),
-    path('login', views.login, name="login")
+    path('login', views.login, name="login"),
+    path('logout', auth_views.LogoutView.as_view(next_page='/'), name="logout"),
 ]
