204a0fb12eed959220f30594c22c99e3e67b24e6
Hendrik Willem Rossouw <synergeticfrost@gmail.com>
2024-11-14 12:13:54 +0200

    refactor to add_list_item , multi lists

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -6,6 +6,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from .server_tools import reset_database
 
 MAX_WAIT = 5
@@ -61,3 +62,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.find_element(By.CSS_SELECTOR, 'input[name=email]')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def add_list_item(self, item_text):
+        num_rows = len(self.browser.find_elements(By.CSS_SELECTOR, '#id_list_table tr'))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -28,34 +28,30 @@ class ItemValidationTest(FunctionalTest):
         )
 
         # And she can submit it successfully
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Purchase milk')
+        self.get_item_input_box().clear()
+        self.add_list_item('Purchase milk')
 
         # Perversely, she now decides to submit a second blank list item
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # Again, the browser will not comply
-        self.wait_for_row_in_list_table('1: Purchase milk')
         self.wait_for(
             lambda: self.browser.find_element(By.CSS_SELECTOR, '#id_text:invalid')
         )
 
-        # And she can make it hapy by filling some text in
+        # And she can make it happy by filling some text in
         self.get_item_input_box().send_keys('Make tea')
         self.wait_for(
             lambda: self.browser.find_element(By.CSS_SELECTOR, '#id_text:valid')
         )
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Purchase milk')
-        self.wait_for_row_in_list_table('2: Make tea')
+        self.get_item_input_box().clear()
+        self.add_list_item('Make tea')
 
     def test_cannot_add_duplicate_items(self):
 
         # Edith goes to the home page and starts a new list
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Buy wellies')
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy wellies')
+        self.add_list_item('Buy wellies')
 
         # She accidentally tries to enter a duplicate item
         self.get_item_input_box().send_keys('Buy wellies')
@@ -73,9 +69,7 @@ class ItemValidationTest(FunctionalTest):
 
         # Edith starts a list and causes a validation error:
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Banter too thick')
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.add_list_item('Banter too thick')
         self.get_item_input_box().send_keys('Banter too thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for(
